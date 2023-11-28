15fffac1b9aa8689343a38d5778bc03795d8874e
napoles <antonio.napoles@muuklabs.com>
2023-11-28 17:42:24 -0600

    Any

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -7,7 +7,7 @@ NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install nodejs -y
 echo "node -v" && node -v
-sudo apt-get install npm
+sudo apt-get update && sudo apt-get install gcc g++ make
+sudo apt-get update && sudo apt-get install npm
 echo "npm -v" && npm -v
-sudo apt-get install gcc g++ make
-sudo apt-get install -y unzip
+sudo apt-get update && sudo apt-get install -y unzip
